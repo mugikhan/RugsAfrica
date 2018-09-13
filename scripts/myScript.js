aae72b0b9bdb2901d198b8d5a2b5b36ef57f962c
mugheesKhan <mugikhan@gmail.com>
2018-09-13 17:43:01 +0200

    Adding basic cart system

---
+++ scripts/myScript.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#getProductsBtn').click(function(){
+        $.ajax({
+            type: 'GET',
+            url: "https://www.rugsafrica.com/wp-json/wc/v2/products?consumer_key=ck_654be29c9e2046f40d5579178e7c656947db9b55&consumer_secret=cs_4a2835d4a5efdbe6d32e234989fef9843c111b20",
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+            }
+        })
+    });
+    $("#myCart").PayPalCart({business: 'mugikhan@gmail.com',
+        notifyURL: '',
+        virtual: false,
+        quantityupdate: true,
+        currency: 'USD',
+        currencySign: '$',
+        minicartid: 'minicart',
+        persitdays: -1,
+    });
+
+    $(".addToCartBtn").on('click', function(){
+        $card = $(".addToCartBtn").parent();
+        var desc = $card.find("h4").text();
+        var priceText = $card.find(".card-text-sale").text();
+        var parts = priceText.split("R");
+        var price = parseInt(parts[1]);
+        console.log(price);
+        $("#myCart").PayPalCart('add','item1', desc, 1, price, 0);
+    });
+});
