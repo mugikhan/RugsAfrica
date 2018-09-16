0957b2e57caa3f008dbffceaf99662125b0d18e8
mugheesKhan <mugikhan@gmail.com>
2018-09-17 00:51:34 +0200

    Added pages

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('#getProductsBtn').click(function(){
+    /*$('#getProductsBtn').click(function(){
         $.ajax({
             type: 'GET',
             url: "https://www.rugsafrica.com/wp-json/wc/v2/products?consumer_key=ck_654be29c9e2046f40d5579178e7c656947db9b55&consumer_secret=cs_4a2835d4a5efdbe6d32e234989fef9843c111b20",
@@ -28,5 +28,30 @@ $( document ).ready(function() {
         var price = parseInt(parts[1]);
         console.log(price);
         $("#myCart").PayPalCart('add','item1', desc, 1, price, 0);
-    });
+    });*/
+
+    $('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			/*titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" + " target="_blank">image source</a>';
+			}*/
+		},
+		gallery: {
+			enabled: false
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 });
